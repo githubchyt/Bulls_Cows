18b388daa0bb7fe9738df63073647da926e53911
githubchyt <117107050+githubchyt@users.noreply.github.com>
2022-12-31 14:34:08 +0100

    Add files via upload

---
+++ function_bull_cow.py
@@ -0,0 +1,90 @@
+"""
+function_bull_cow.py: Functions for 2nd project in Engeto
+
+author: Lucie Chytilová
+email: studyroomchy@gmail.com
+discord: Lucie Ch.#8282
+"""
+
+from random import sample as rd
+
+def secreat_nbr () -> str:
+    """
+    Program generates secret four-digit number.
+    Number has to be unique and doesn't start zero.
+    """
+
+    number = '0'
+    
+    while number[0] == '0':
+        number = "".join(map(str, rd(range(0, 10), k=4)))
+    
+    return number
+
+
+def check_number (number: str) -> bool:
+    """
+    Check if string is number with these conditions:
+     - nbr has 4 digits
+     - nbr doesn't contain duplicity
+     - nbr doesn't start zero
+    """
+    
+    return (len(number) == 4 and len(set(number)) == 4 and 
+            number[0] != '0' and number.isdigit())
+
+
+def eval_bull_cow (nbr1: str, nbr2: str) -> tuple:
+    """
+    Function compares two string for every position and
+    writes out count of bull/bulls or cow/cows.
+    
+    Bull = value and position are same for both string
+    Cow = value is in 2nd string but another position
+    
+    Example:
+    nbr1 = '2598'
+    nbr2 = '5092'
+    
+    Result: ('1 bull', '2 cows')
+    bull = 1 (for '9')
+    cow = 2 (for '2' and '5')
+    
+    """
+
+    score = dict(bull = 0, cow = 0)
+    
+    for i in range(len(nbr1)):
+        if nbr1[i] == nbr2[i]:
+            score['bull'] += 1
+        else:
+            if nbr1[i] in nbr2:
+                score['cow'] += 1
+    
+    if score.get('bull') == 1 and score.get('cow') == 1:
+        text = ('bull', 'cow')
+    elif score.get('bull') == 1 and score.get('cow') > 1:
+        text = ('bull', 'cows')
+    elif score.get('bull') > 1 and score.get('cow') == 1:
+        text = ('bulls', 'cow')
+    else:
+        text = ('bulls', 'cows')
+    
+    return f"{score.get('bull')} {text[0]}", f"{score.get('cow')} {text[1]}"
+
+
+def eval_text (count: int) -> str:
+    """
+    Functions returns an evaluation based on the number of guesses.
+    """
+
+    if count in range(1,4):
+        text = 'amazing'
+    elif count in range (4,7):
+        text = 'average'
+    elif count in range (7,11):
+        text = 'not so bad'
+    else:
+        text = 'bad luck. Better luck next time'
+    return text
+
---
+++ project_2_lchy.py
@@ -0,0 +1,97 @@
+"""
+project_2_lchy.py: 2nd project in Engeto Online Python Academy
+
+author: Lucie Chytilová
+email: studyroomchy@gmail.com
+discord: Lucie Ch.#8282
+"""
+
+import time
+import function_bull_cow as fbc
+
+SEP = "------------------------------------------------------------------"
+
+game = 'Y'
+stats = dict()
+count_play = 0
+
+print(f"Hi there!")
+
+while game.upper() == 'Y':
+    count_play += 1
+    
+    # create secreate number
+    secreat_number = fbc.secreat_nbr()
+    
+    # you can show generated secrate number
+    # print("Secret number is: ", secreat_number)
+    
+    print(SEP)
+    print("I've generated a random 4 digit number for you.")
+    print("Let's play a bulls and cows game.")
+    print(SEP) 
+    print("Enter a number (max 4 digits, not start 0, all digits unique):")
+    print("If you want to quit enter 'Q' instead of the number.")
+    print(SEP)
+        
+    start = time.time()
+    count_guess = 0
+    guess_number = ''  
+    
+    while guess_number != secreat_number and guess_number.upper() != 'Q':
+        guess_number = input(">>> ")
+        
+        if guess_number.upper() != 'Q':
+            
+            # check if guess number has correct format
+            if fbc.check_number(guess_number):    
+                count_guess +=1
+                
+                # compare guess number and generated secrate number
+                result = fbc.eval_bull_cow(guess_number, secreat_number)
+                
+                # payer guesses secrate number
+                if result[0].split()[0] == '4': 
+                    endt = time.time()
+                    
+                    stats[count_play]=[count_guess, time.strftime("%H:%M:%S", 
+                    time.gmtime(endt-start)), fbc.eval_text(count_guess)]
+       
+                    print(f"Correct, you've guessed the right number in "
+                        f"{count_guess} guesses!")
+                    
+                    print(f"That's {fbc.eval_text(count_guess)}!")
+                    print(SEP)
+                    
+                    game = input("If you want to play new next game,"
+                        "press 'Y': ")
+
+                # payer doesn't guess secrate number                  
+                else:
+                    print(', '.join(result))
+                    print(SEP)
+                    
+            else:
+                print(f"Your number {guess_number} has wrong format.")
+                print(SEP)
+        else:    
+            print(f"Secret number is: {secreat_number}")
+            print(f"You lose your {count_play}. game!")
+            game = 'N'
+   
+# writes out game statistics
+if len(stats) != 0:     
+    print(f"\n{'-'*90}\n|{'YOUR SCORE':^88}|\n{'-'*90}")
+    
+    print(f"|{'Game Nbr.':^15}|{'Nbr. of Guesess':^15}|{'Game Time':^15}|"
+        f"{'Evaluation':^40}|\n{'-'*90}")
+   
+    for key, values in stats.items():
+        count_guess, time, fbc.eval_text = values
+        
+        print(f"|{key:^15}|{count_guess:^15}|{time:^15}|"
+            f"{'''That's '''+ fbc.eval_text:^40}|")
+
+    print('-'*90)
+       
+     
